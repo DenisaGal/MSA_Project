2fa6e3eabccd63319ef5286790321ab3e1df21f1
Deni <denisagal09@yahoo.com>
2021-11-29 21:05:31 +0200

    Encrypting password before adding in the database

--- PianoApp/Assets/Register/Register.cs
+++ PianoApp/Assets/Register/Register.cs
@@ -11,12 +11,15 @@ public class Register : MonoBehaviour
 	public InputField confirmPassword;
 
 	DB pianoDB;
+	//EncryptPassword hashPassword;
 
     // Start is called before the first frame update
     void Start()
     {
     	pianoDB = GameObject.FindGameObjectWithTag("PDB").GetComponent<DB>();
         pianoDB.CreatePianoDB();
+
+        //hashPassword = GameObject.FindGameObjectWithTag("Encrypt").GetComponent<EncryptPassword>();
     }
 
     // Update is called once per frame
@@ -25,11 +28,10 @@ public class Register : MonoBehaviour
         
     }
 
-    //!!!!!!!!DO NOT FORGET TO HASH PWD!!!!!!!!
     //!!!!!!!!DO NOT FORGET TO HANDLE CONSTRAINT VIOLATIONS VIA POP UPS!!!!!!!!
-    public void RegisterUser(){//(string email, string username, string password, string confirmPassword){
+    public void RegisterUser(){
     	if(string.Equals(password.text, confirmPassword.text)){
-    		pianoDB.AddUser(pianoDB.getLastUserID() + 1, email.text, username.text, password.text);
+    		pianoDB.AddUser(pianoDB.getLastUserID() + 1, email.text, username.text, EncryptPassword.HashString("aaaaaaa"));
     		Debug.Log("User added.\n");
     	}
     	else{
