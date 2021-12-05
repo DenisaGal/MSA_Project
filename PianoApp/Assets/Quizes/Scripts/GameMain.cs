2f033bd959e7bb82ce7dad7845f70e816eca7adb
ioana-lazar <62338641+ioana-lazar@users.noreply.github.com>
2021-12-05 17:25:04 +0200

    Quiz answers and load next question

--- PianoApp/Assets/Quizes/Scripts/GameMain.cs
+++ PianoApp/Assets/Quizes/Scripts/GameMain.cs
@@ -1,14 +1,18 @@
+//Thank you Brackeys <3
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class GameMain : MonoBehaviour
 {
     public Question[] questions;
     private Question currentQuestion;
     public Text result;
+    [SerializeField] private float delay = 2f;
     void Start()
     {
         GetRandomQuestion();
@@ -22,10 +26,25 @@ public class GameMain : MonoBehaviour
         currentQuestion.displayedNote.SetActive(true);
     }
 
+    IEnumerator TransitionToNextQuestion()
+    {
+        yield return new WaitForSeconds(delay);
+        
+        currentQuestion.displayedNote.SetActive(false);
+        result.text = "";
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
     public void UserSelect(string userAnswer)
     {
+
         if (userAnswer == currentQuestion.answer)
             result.text = "Correct!"; //should get xp here ? or at the end
         else result.text = "Wrong :(";
+
+        StartCoroutine(TransitionToNextQuestion());
+        
+        //add colours green/red la raspunsuri
+        //cum facem cu xp? cand primeste
     }
 }
