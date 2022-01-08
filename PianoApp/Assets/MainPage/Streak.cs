998e0e2fd559eb466b93c3e23f5022fc7dea2680
Deni <denisagal09@yahoo.com>
2022-01-09 00:27:16 +0200

    Initial display streak.

--- PianoApp/Assets/MainPage/Streak.cs
+++ PianoApp/Assets/MainPage/Streak.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Streak : MonoBehaviour
 {
+	DB pianoDB;
+
+	public Text streak;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    	pianoDB = GameObject.FindGameObjectWithTag("PDB").GetComponent<DB>(); 
+    	
+        showStreak();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void showStreak(){
+    	streak.text = "5";
+        //streak.text = Convert.ToString(pianoDB.getUserStreak(Login.currentUsername));
     }
 }
